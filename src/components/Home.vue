24b080d6bc63222ae327096fcf0c4132c396eae4
whitestapler <joyyang34@g.ucla.edu>
2021-04-27 14:38:20 -0700

    trying to deploy to netlify

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="Home">
-    Home
+    Home!
   </div>
 </template>
 
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/white-stapler/public'
+      ? '/white-stapler/dist'
       : '/'
   }
